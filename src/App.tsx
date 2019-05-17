e01024c823470d69964c4b1159aa5ddbc9467ab4
Thomas Johnson <tjohnson1106@gmail.com>
2019-05-17 17:42:05 -0400

    created initial router configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "../node_modules/materialize-css/dist/js/materialize.min.css";
+import "../node_modules/materialize-css/dist/js/materialize.min.js";
+import "./App.css";
+
+import { NavBar } from "./components/NavBar";
+import { About } from "./pages/about";
+import { Home } from "./pages/home";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <div className="container">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
